4dd877b437cf21f67b39bae9f6ca9781df09ac73
Katja Kjer <Kalle3008@hotmail.com>
2018-04-16 09:23:04 +0200

    java + progressbar

---
+++ js/lektionkommentar.js
@@ -0,0 +1,56 @@
+function setLocal(localForumposts){
+    var jsonData = JSON.stringify(localForumposts);
+
+    localStorage.setItem("lektionkommentar", jsonData);
+}
+
+function getLocal(){
+    var lektionkommentar = localStorage.getItem("lektionkommentar");
+    if(lektionkommentar == null){
+        return [];
+    }else{
+        return JSON.parse(lektionkommentar);
+    }
+}
+
+function submitNote(text){
+    var origPosts = getLocal();
+
+    var newComment = {
+        text:       text
+    };
+
+    origPosts.push(newComment);
+
+    setLocal(origPosts);
+}
+
+function buildList(){
+    var noteList = getLocal();
+    var ulElm = document.querySelector("#forumKommentar");
+
+    ulElm.innerHTML = "";
+
+    for(var i = 0; i < noteList.length; i++){
+        var liElm = document.createElement("li");
+        liElm.setAttribute("class", "kommentar_lektion center");
+
+        liElm.innerHTML = "<img class='kommentar_ikon' src='img/header/profil.png'><p class='kommentar_forfatter'> Bjarne S. </p><p class='kommentar_tekst'>" + noteList[i].text + "</p>";
+
+        ulElm.appendChild(liElm);
+    }
+}
+
+function submitNoteEvent(event) {
+    var noteText = document.querySelector("#kommentar");
+
+    submitNote(noteText.value);
+    buildList();
+}
+
+window.onload = function(){
+    buildList();
+}
+
+var submitNoteBtn = document.querySelector("#addComment");
+submitNoteBtn.addEventListener("click", submitNoteEvent);
